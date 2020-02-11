5aebfa9267ffebb1f8a0794614b095ccc9d11671
Beth <bethhnielsen56@gmail.com>
2020-02-10 21:18:58 -0600

    wip

---
+++ main.js
@@ -0,0 +1,29 @@
+newsArr = [
+    {
+    date: '01/01/2020',
+    imageUrl: "something.jpg",
+    desc: "lorem ipsum"
+    },
+    {
+    date: '01/01/2020',
+    imageUrl: "something.jpg",
+    desc: "lorem ipsum"
+    },
+    {
+    date: '01/01/2020',
+    imageUrl: "something.jpg",
+    desc: "lorem ipsum"
+    },
+    {
+    date: '01/01/2020',
+    imageUrl: "something.jpg",
+    desc: "lorem ipsum"
+    } 
+]
+
+
+function makeNewsCards = () => {
+    for (let i = 0; i < newsArr.length; i++) {
+      
+    }
+}
