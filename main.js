f1e41882a4e9934600c8536bb861a2e4a54736d9
Olamide Oladinni <olajide1987@gmail.com>
2020-02-11 21:34:52 -0600

    add cards

--- main.js
+++ main.js
@@ -1,3 +1,61 @@
+//Arrays//
+tourDates = [{
+    date: "02/28/2020",
+    city: "Sacramento",
+    state: "CA",
+    venue: "The Dungeon"},
+
+    {date: "03/01/2020",
+    city: "San francisco",
+    state: "CA",
+    venue: "The Devils Playground",
+    },
+
+    {date: "03/03/2020",
+    city: "SanDiego",
+    state: "CA",
+    venue: "The Labyrinth",
+    },
+
+    {date: "03/05/2020",
+    city: "Los Angeles",
+    state: "CA",
+    venue: "Skid Row",
+    },
+
+    {date: "03/05/2020",
+    city: "Las Vegas",
+    state: "NV",
+    venue: "Sin City",
+    },
+
+    {date: "03/09/2020",
+    city: "Reno",
+    state: "NV",
+    venue: "The Bunny Ranch",
+    }
+]
+
+
+const makeTourCards = (tourDates,divId) => {
+    let Domstring = '';
+for(let i = 0; i < tourDates.length; i++) {
+    Domstring =+ '<div class="tour-container">';
+    Domstring =+ `<h5 class="card-header">Featured</h5>`;
+    Domstring =+  <div class="card-body">
+    Domstring =+  <h5 class="card-title">Special title treatment</h5>
+    Domstring =+  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    Domstring =+  </div>
+    Domstring =+  </div>
+}
+
+}
+
+
+
+
+
+
 // SUBSCRIPTION FORM FUNCTION
 const emailConfirm = () => {
     const textInput = document.getElementById('exampleInputEmail1')
@@ -13,6 +71,8 @@ const emailConfirm = () => {
 const events = () => {
     document.getElementById('emailButton').addEventListener('click', emailConfirm )
 };
+
+
 
 // INITIAL FUNCTION
 const init = () => {
