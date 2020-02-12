0f6e32fc85197f2f2101b245ab185444f9ca00ed
Beth <bethhnielsen56@gmail.com>
2020-02-11 18:54:05 -0600

    wip

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
-newsArr = [
+const newsArr = [
     {
     date: '01/01/2020',
     imageUrl: "something.jpg",
-    desc: "lorem ipsum"
+    description: "lorem ipsum"
     },
     {
     date: '01/01/2020',
     imageUrl: "something.jpg",
-    desc: "lorem ipsum"
+    description: "lorem ipsum"
     },
     {
     date: '01/01/2020',
     imageUrl: "something.jpg",
-    desc: "lorem ipsum"
+    description: "lorem ipsum"
     },
     {
     date: '01/01/2020',
     imageUrl: "something.jpg",
-    desc: "lorem ipsum"
+    description: "lorem ipsum"
     } 
 ]
 
 
-function makeNewsCards = () => {
+const makeNewsCards = (newsArr) => {
+    let domString = ""
     for (let i = 0; i < newsArr.length; i++) {
-      
+        domString += `<div class="card">`;
+        domString +=   `<div class="card-body">`;
+        domString +=      `<img src="${newsArr[i].imageUrl}"`;
+        domString +=      `<h3>${newsArr[i].date}</h3>`;
+        domString +=      `<h4>${newsArr[i].description}</h4>`;
+        domString +=   `</div>`;
+        domString += `</div>`;
     }
+    printToDom("newsCards", domString);
 }
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+}
+
+const init = () => {
+    makeNewsCards();
+}
+
+makeNewsCards();
