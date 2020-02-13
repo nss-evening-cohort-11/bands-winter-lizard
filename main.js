8b5294db371daddf3446dc8c4048253309845d81
Joey Petrone <joey.petrone@gmail.com>
2020-02-12 22:59:38 -0600

    Added function and array for bio cards

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
+// NEWS ARRAY
+const newsArrInfo = [
+    {
+    title: "Our Newest addition to the Band",
+    date: '03/31/2013',
+    imageUrl: "https://i.pinimg.com/originals/99/33/8b/99338be9bba30c2cc64682c79861964c.jpg",
+    description: "We wash our brush with odorless thinner. We start with a vision in our heart, and we put it on canvas. Let's put a touch more of the magic here. Let your imagination be your guide. If it's not what you want - stop and change it. Don't just keep going and expect it will get better."
+    },
+    {
+    title: "New Album!",
+    date: '02/23/2013',
+    imageUrl: "https://media.npr.org/assets/img/2018/03/11/ap_18046670213384_wide-b52b8fe4baa52b6768a875605f8c516a2177d807-s800-c85.jpg",
+    description: "We are excited to announce a new album coming out on 07/04/2013!! We hope to have you join us at Waffle House to celebrate!  This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart. I will take some magic white, and a little bit of Vandyke brown and a little touch of yellow. In this world, everything can be happy."
+    },
+    {
+    title: "Local Show Coming up!",
+    date: '01/15/2013',
+    imageUrl: "https://www.straitstimes.com/sites/default/files/styles/article_pictrure_780x520_/public/articles/2019/03/22/jtoncert220319.jpg?itok=x-woy2uM&timestamp=1553254399",
+    description: "Anytime you learn something your time and energy are not wasted. Every highlight needs it's own personal shadow. Let's make a happy little mountain now. If there's two big trees invariably sooner or later there's gonna be a little tree. You need the dark in order to show the light. There comes a nice little fluffer."
+    },
+    {
+    title: "Just another thing coming up...",
+    date: '08/10/2012',
+    imageUrl: "https://i.pinimg.com/originals/d2/c2/d5/d2c2d515066110a54c720174f1cba496.jpg",
+    description: "We can fix anything. Maybe there's a little something happening right here. We need dark in order to show light. There he comes. Just think about these things in your mind - then bring them into your world. Just take out whatever you don't want. It'll change your entire perspective."
+    } 
+];
+
 // MEMBER BIOS ARRAY
 const bios = [
     {
-        imageUrl: "https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/11188388_855799771122467_8248206020299838593_n.jpg?_nc_cat=107&_nc_ohc=K4mALcX0AMEAX_BbIdz&_nc_ht=scontent-den4-1.xx&oh=68ad399803320dffbfa1a845c7ae9fe6&oe=5EBAFFB6",
-        name: "WinterLizard",
-        role: "About Us",
-        bio: "Mistakes have been made. You’re going to be home by 8, listening to the Talking Heads and reading Vonnegut or something. That sounds like a nice night. Um, I'm happy you're home. YOU BETTER RUN! She's our friend, and she's crazy! Hey kiddo, would you like a balloon?"
-    },
-    {
-        imageUrl: "https://scontent-den4-1.xx.fbcdn.net/v/t31.0-8/12695058_979043065464803_6165821191574967721_o.jpg?_nc_cat=106&_nc_ohc=tEBFQMTUOz0AX_Ssiw7&_nc_ht=scontent-den4-1.xx&oh=f57e9c72d699f4aa4eb7b79a27d94690&oe=5F01A2E7",
+        imageUrl: "https://scontent.fbna1-2.fna.fbcdn.net/v/t1.0-9/10678579_749345165101262_849584611814256163_n.jpg?_nc_cat=109&_nc_ohc=UZkYhRGgLlwAX_T61_T&_nc_ht=scontent.fbna1-2.fna&oh=aceeeb5d104fd4e22ff27c93145ce774&oe=5F03A3E0",
         name: "David",
         role: "Vocals & Lyrics",
         bio: "As far as we can estimate, some bronzy blades are thought of simply as surprises. The pulsing albatross comes from a negroid relation. A licit current's trip comes with it the thought that the plumaged committee is a plywood. To be more specific, some glottic profits are thought of simply as peaces."
@@ -38,34 +60,6 @@ const bios = [
     }
 ];
 
-// NEWS ARRAY
-const newsArrInfo = [
-    {
-    title: "Our Newest addition to the Band",
-    date: '03/31/2013',
-    imageUrl: "https://i.pinimg.com/originals/99/33/8b/99338be9bba30c2cc64682c79861964c.jpg",
-    description: "We wash our brush with odorless thinner. We start with a vision in our heart, and we put it on canvas. Let's put a touch more of the magic here. Let your imagination be your guide. If it's not what you want - stop and change it. Don't just keep going and expect it will get better."
-    },
-    {
-    title: "New Album!",
-    date: '02/23/2013',
-    imageUrl: "https://media.npr.org/assets/img/2018/03/11/ap_18046670213384_wide-b52b8fe4baa52b6768a875605f8c516a2177d807-s800-c85.jpg",
-    description: "We are excited to announce a new album coming out on 07/04/2013!! We hope to have you join us at Waffle House to celebrate!  This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart. I will take some magic white, and a little bit of Vandyke brown and a little touch of yellow. In this world, everything can be happy."
-    },
-    {
-    title: "Local Show Coming up!",
-    date: '01/15/2013',
-    imageUrl: "https://www.straitstimes.com/sites/default/files/styles/article_pictrure_780x520_/public/articles/2019/03/22/jtoncert220319.jpg?itok=x-woy2uM&timestamp=1553254399",
-    description: "Anytime you learn something your time and energy are not wasted. Every highlight needs it's own personal shadow. Let's make a happy little mountain now. If there's two big trees invariably sooner or later there's gonna be a little tree. You need the dark in order to show the light. There comes a nice little fluffer."
-    },
-    {
-    title: "Just another thing coming up...",
-    date: '08/10/2012',
-    imageUrl: "https://i.pinimg.com/originals/d2/c2/d5/d2c2d515066110a54c720174f1cba496.jpg",
-    description: "We can fix anything. Maybe there's a little something happening right here. We need dark in order to show light. There he comes. Just think about these things in your mind - then bring them into your world. Just take out whatever you don't want. It'll change your entire perspective."
-    } 
-]
-
 // PRINT TO DOM
 const printToDom = (divId, textToPrint) => {
     const selectedId = document.getElementById(divId)
@@ -88,23 +82,25 @@ const makeNewsCards = () => {
         domString += `</div>`;
     }
     printToDom("newsCards", domString);
-}
+};
 
 // MAKE MEMBER BIO CARDS
 const makeBioCards = () => {
     let domString = '';
     for ( i = 0; i < bios.length; i++ ) {
-        domString += '<div class="bios-card d-flex justify-content-center text-dark m-3">'
-        domString += '   <div class="card-image">'
-        domString += `      <img src="${bios[i].imageUrl}" alt="${bios[i].name} playing music on stage">`
-        domString += '   </div>'
-        domString += '   <div class="card">'
-        domString += `      <h5 class="card-header">${bios[i].name}</h5>`
-        domString += '      <div class="card-body">'
-        domString += `          <h5 class="card-title">${bios[i].role}</h5>`
-        domString += `          <p class="card-text">${bios[i].bio}</p>`
-        domString += '      </div>'
-        domString += '   </div>'
+        domString += '<div class="card d-flex justify-content-center bg-dark ml-5 mr-5 mt-4 mb-4">'
+        domString += '   <div class="row no-gutters">'
+        domString += '       <div class="col-md-2">'
+        domString += `          <img src="${bios[i].imageUrl}" class="card card-img bg-dark" alt="${bios[i].name} playing music on stage">`
+        domString += '       </div>'
+        domString += '       <div class="card col-md-10 bg-dark text-white">'
+        domString += `          <h5 class="card-header">${bios[i].name}</h5>`
+        domString += '          <div class="card-body">'
+        domString += `              <h6 class="card-title">${bios[i].role}</h6>`
+        domString += `              <p class="card-text">${bios[i].bio}</p>`
+        domString += '          </div>'
+        domString += '       </div>'
+        domString += '   </div>' 
         domString += '</div>'
     }
     printToDom('bio-container', domString)
@@ -122,16 +118,28 @@ const emailConfirm = () => {
     }
 };
 
-// EVENTS
-const events = () => {
-    document.getElementById('emailButton').addEventListener('click', emailConfirm )
-};
-
 // INITIAL FUNCTION
 const init = () => {
-    //events();
-    //makeNewsCards();
-    makeBioCards();
+
+    // LOCATION SWITCH STATEMENT
+    switch (window.location.href) {
+        case 'http://localhost:8080/index.html':
+            document.getElementById('emailButton').addEventListener('click', emailConfirm );
+            makeNewsCards();
+            break; 
+        case 'http://localhost:8080/members.html':
+            makeBioCards();
+            break;    
+        case 'http://localhost:8080/merch.html':
+            console.log('merch');
+            break; 
+        case 'http://localhost:8080/music.html':
+            console.log('music');
+            break;  
+        case 'http://localhost:8080/tour.html':
+            console.log('tour');
+            break;             
+    }
 };
 
 init();
